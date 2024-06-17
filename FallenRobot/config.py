a573133b3106ebfc2872fd705b8a6e91d9b8b578
82kpro_Official <161997419+UNNI12345YT@users.noreply.github.com>
2024-06-17 11:16:03 +0530

    Update config.py
    Signed-off-by: 82kpro_Official <161997419+UNNI12345YT@users.noreply.github.com>

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -2,8 +2,8 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    API_ID = 6
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+    API_ID = 27408015
+    API_HASH = "2f07e7c921c8d2b982df12d65a46ca46"
 
     CASH_API_KEY = ""  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
 
@@ -14,7 +14,7 @@ class Config(object):
     MONGO_DB_URI = ""  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
-    START_IMG = "https://te.legra.ph/file/40eb1ed850cdea274693e.jpg"
+    START_IMG = "https://telegra.ph/file/2858cba6f9ba1a437b947.mp4"
 
     SUPPORT_CHAT = "DevilsHeavenMF"  # Your Telegram support group chat username where your users will go and bother you
 
@@ -22,7 +22,7 @@ class Config(object):
 
     TIME_API_KEY = ""  # Get this value from https://timezonedb.com/api
 
-    OWNER_ID = 1356469075  # User id of your telegram account (Must be integer)
+    OWNER_ID = 6171681404  # User id of your telegram account (Must be integer)
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
